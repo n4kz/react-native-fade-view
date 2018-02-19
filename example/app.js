9822b7ba6b99ade6689c04a71f966d9fa74bb90f
n4kz <n4kz@n4kz.com>
2018-02-19 13:11:05 +0300

    Updated example

--- example/app.js
+++ example/app.js
@@ -21,43 +21,42 @@ export default function init() {
       let { active } = this.state;
 
       return (
-        <View style={{ flex: 1, backgroundColor: '#01579B' }}>
+        <View style={styles.container}>
           <FadeView style={{ flex: 1 }} animationDuration={450} active={active}>
             <WaveIndicator color='white' count={3} size={80} />
-            <Text style={{ color: 'rgba(255, 255, 255, 0.54)', backgroundColor: 'transparent', fontSize: 17 }}>
-              <Text style={{ color: 'rgba(255, 255, 255, 0.87)', fontWeight: '500' }}>FadeView</Text> is in active state
+            <Text style={styles.text}>
+              <Text style={styles.highlight}>FadeView</Text> is in active state
             </Text>
-            <Text style={{ color: 'rgba(255, 255, 255, 0.54)', backgroundColor: 'transparent', fontSize: 21 }}>
-              <Text style={{ color: 'rgba(255, 255, 255, 0.87)', fontWeight: '500' }}>FadeView</Text> displaying 2
-            </Text>
-            <Text style={{ color: 'rgba(255, 255, 255, 0.54)', backgroundColor: 'transparent', fontSize: 23 }}>
-              <Text style={{ color: 'rgba(255, 255, 255, 0.87)', fontWeight: '500' }}>FadeView</Text> displaying 3
+            <Text style={styles.text}>
+              <Text style={styles.highlight}>FadeView</Text> shows alternative text
             </Text>
           </FadeView>
 
-          <View style={{ padding: 32, backgroundColor: 'white' }}>
+          <View style={styles.buttonContainer}>
             <RaisedTextButton
               color='#01579B'
-              title='toggle state'
+              style={styles.button}
+              title='set inactive'
               titleColor='white'
-              onPress={() => this.setState(({ active }) => ({ active: !active }))}
-              rippleDuration={800}
+              onPress={() => this.setState({ active: 0 })}
               rippleSequential={false}
             />
+
             <RaisedTextButton
               color='#01579B'
-              title='set index 2'
+              style={styles.button}
+              title='set active'
               titleColor='white'
-              onPress={() => this.setState(({ active }) => ({ active: 2 }))}
-              rippleDuration={800}
+              onPress={() => this.setState({ active: 1 })}
               rippleSequential={false}
             />
+
             <RaisedTextButton
               color='#01579B'
-              title='set index 3'
+              style={styles.button}
+              title='show alternative'
               titleColor='white'
-              onPress={() => this.setState(({ active }) => ({ active: 3 }))}
-              rippleDuration={800}
+              onPress={() => this.setState({ active: 2 })}
               rippleSequential={false}
             />
           </View>
@@ -68,3 +67,30 @@ export default function init() {
 
   AppRegistry.registerComponent('example', () => Example);
 }
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#01579B',
+  },
+
+  text: {
+    color: 'rgba(255, 255, 255, 0.54)',
+    backgroundColor: 'transparent',
+    fontSize: 17,
+  },
+
+  highlight: {
+    color: 'rgba(255, 255, 255, 0.87)',
+    fontWeight: '500',
+  },
+
+  button: {
+    margin: 8,
+  },
+
+  buttonContainer: {
+    padding: 8,
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+  },
+};
